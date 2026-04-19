62753e8d2e1b84f4bc5b20edc4506c28b5a1e467
Hermes <hermes8agistrian03@gmail.com>
2026-04-19 15:57:27 +0700

    setup shadcn system design and react router dom

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TestPage from './pages/TestPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/test" element={<TestPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function TestPage() {
+    return (
+        <>
+            <Button>Test Page</Button>
+        </>
+    )
+}
