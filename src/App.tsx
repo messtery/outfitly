653ab35267ec01c4b5a04533f30d3762baf1e472
R.S <evanaweken@gmail.com>
2026-04-25 20:46:19 +0700

    Perbaikan Cart dan Chekout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Register from './pages/Register.jsx';
 import Menu from './pages/Menu.jsx';
 import CartPage from './pages/CartPage.jsx';
 import CheckoutPage from './pages/CheckoutPage.jsx';
-import OrderTrackingPage from './pages/OrderTrackingPage'
+
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           <Route path="/menu" element={<Menu />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/ordertracking" element={<OrderTrackingPage />} />
+          
         </Routes>
       </BrowserRouter>
   );
