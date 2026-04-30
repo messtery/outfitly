545bac509bf3871d50f6136742f4d3014f2b520e
frezzliodas <frederickosasih@gmail.com>
2026-05-01 01:13:15 +0700

    Add files via upload

--- api/db.js
+++ api/db.js
@@ -9,11 +9,4 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     dialect: 'mysql',
 });
 
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-} catch (error) {
-    console.error('Unable to connect to the database:', error);
-}
-
-export default sequelize
+export default sequelize;
