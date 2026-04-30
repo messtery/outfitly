f5818339396e7df01581ef801fe71e3da80fa449
frezzliodas <frederickosasih@gmail.com>
2026-05-01 01:26:40 +0700

    Add files via upload

--- api/routes/customerRoutes.js
+++ api/routes/customerRoutes.js
@@ -1,1 +1,12 @@
+import express from 'express';
+import * as ctrl from '../controllers/customerController.js';
 
+const router = express.Router();
+
+router.post('/', ctrl.create);
+router.get('/', ctrl.findAll);
+router.get('/:id', ctrl.findOne);
+router.put('/:id', ctrl.update);
+router.delete('/:id', ctrl.remove);
+
+export default router;
